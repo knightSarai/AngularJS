25e0722ae79e72e5a9ab07450f72aa0b5813a0a8
knightSarai <knight@DESKTOP-EP69NRM.localdomain>
2020-11-10 17:17:53 +0200

    10 - 11 - 2020

---
+++ app/app.js
@@ -0,0 +1,50 @@
+let knightApp = angular.module('knightApp', [])
+
+// knightApp.config(function(){
+
+// });
+
+// knightApp.run(function(){
+
+// });
+
+function knightContFunc($scope) {
+    $scope.message = "knight";
+    $scope.removePerson = (person) => {
+        $scope.persons = $scope.persons.filter(p => p !== person)
+    }
+    $scope.addNewPerson = () => {
+        const newPerson = {
+            name: $scope.newPerson.name,
+            money: parseInt($scope.newPerson.money),
+            status: $scope.newPerson.status,
+            available: true
+        }
+        $scope.persons = [...$scope.persons, newPerson];
+        $scope.newPerson.name = '';
+        $scope.newPerson.money = '';
+        $scope.newPerson.status = '';
+    }
+    $scope.persons = [
+        {
+            name: 'knight',
+            money: 500,
+            status: 'in relation two ways',
+            available: true
+        },
+        {
+            name: 'sarah',
+            money: 350,
+            status: 'in relation on way',
+            available: true
+        },
+        {
+            name: 'ward',
+            money: 0,
+            status: 'in relation one way',
+            available: false
+        }
+    ]
+}
+
+knightApp.controller('KnightController', ['$scope', knightContFunc]);
